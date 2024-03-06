a68a43ef0dd44af536d7cbb9d6321d02a3e2c380
Kostiantyn Volobuiev <volobuiev.kostiantyn@gmail.com>
2024-03-07 00:29:02 +0200

    [LocationTrackingService] Android example was taken

--- OpenYourWorldAndroid/app/build.gradle.kts
+++ OpenYourWorldAndroid/app/build.gradle.kts
@@ -37,12 +37,18 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.compose.animation:animation:1.6.3")
+    implementation("androidx.compose.material:material:1.6.3")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.accompanist:accompanist-permissions:0.35.0-alpha")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/LocationTrackingService.kt
+++ OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/LocationTrackingService.kt
@@ -1,6 +1,146 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.openyourworld
 
-class LocationTrackingService {
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import org.w3c.dom.Text
+import java.lang.reflect.Modifier
+import java.util.concurrent.TimeUnit
+
+// Implement a background service for continuous location tracking.
+// Use the Android Location API to obtain location updates.
+// based on this example https://github.com/android/platform-samples/tree/main/samples/location/src/main/java/com/example/platform/location/bglocationaccess
+class LocationTrackingService(context: Context, param: WorkerParameters) :
+    CoroutineWorker(context, param) {
     private val TAG: String = LocationTrackingService::class.java.getSimpleName();
-    // Todo: check example implementation from https://github.com/gonpuga/LocationUpdatesForegroundService
+
+    companion object {
+        // unique name for the work
+        val workName = "LocationTrackingService"
+        private const val TAG = "BackgroundLocationWork"
+    }
+    private val locationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    override suspend fun doWork(): Result {
+        if (ActivityCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return Result.failure()
+        }
+        locationClient.getCurrentLocation(
+            Priority.PRIORITY_BALANCED_POWER_ACCURACY, CancellationTokenSource().token,
+        ).addOnSuccessListener { location ->
+            location?.let {
+                Log.d(
+                    TAG,
+                    "Current Location = [lat : ${location.latitude}, lng : ${location.longitude}]",
+                )
+            }
+        }
+        return Result.success()
+    }
+    fun BgLocationAccessScreen() {
+        // Request for foreground permissions first
+        PermissionBox(
+            permissions = listOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            requiredPermissions = listOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            onGranted = {
+                // From Android 10 onwards request for background permission only after fine or coarse is granted
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    PermissionBox(permissions = listOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                        BackgroundLocationControls()
+                    }
+                } else {
+                    BackgroundLocationControls()
+                }
+            },
+        )
+    }
+
+    private fun BackgroundLocationControls() {
+        val context = LocalContext.current
+        val workManager = WorkManager.getInstance(context)
+
+        // Component UI state holder
+        data class ControlsState(val text: String, val action: String, val onClick: () -> Unit)
+
+        // Observe the worker state to show enable/disable UI
+        val workerState by workManager.getWorkInfosForUniqueWorkLiveData(LocationTrackingService.workName)
+            .observeAsState()
+
+        val controlsState = remember(workerState) {
+            // Find if there is any enqueued or running worker and provide UI state
+            val enqueued = workerState?.find { !it.state.isFinished } != null
+            if (enqueued) {
+                ControlsState(
+                    text = "Check the logcat for location updates every 15 min",
+                    action = "Disable updates",
+                    onClick = {
+                        workManager.cancelUniqueWork(LocationTrackingService.workName)
+                    },
+                )
+            } else {
+                ControlsState(
+                    text = "Enable location updates and bring the app in the background.",
+                    action = "Enable updates",
+                    onClick = {
+                        // Schedule a periodic worker to check for location every 15 min
+                        workManager.enqueueUniquePeriodicWork(
+                            LocationTrackingService.workName,
+                            ExistingPeriodicWorkPolicy.KEEP,
+                            PeriodicWorkRequestBuilder<LocationTrackingService>(
+                                15,
+                                TimeUnit.MINUTES,
+                            ).build(),
+                        )
+                    },
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = controlsState.text)
+            Button(onClick = controlsState.onClick) {
+                Text(text = controlsState.action)
+            }
+        }
+    }
 }
