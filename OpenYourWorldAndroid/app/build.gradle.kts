e5075d8e5509fda7c5e10c106071246e1efca1b5
Kostiantyn Volobuiev <volobuiev.kostiantyn@gmail.com>
2024-05-20 01:43:45 +0300

    [Fix] Composable method call error was fixed

--- OpenYourWorldAndroid/app/build.gradle.kts
+++ OpenYourWorldAndroid/app/build.gradle.kts
@@ -41,20 +41,17 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("androidx.compose.animation:animation:1.6.4")
-    implementation("androidx.compose.material:material:1.6.4")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("com.google.accompanist:accompanist-permissions:0.35.0-alpha")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/FirstFragment.kt
+++ OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.example.openyourworld
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.navigation.fragment.findNavController
 import com.example.openyourworld.databinding.FragmentFirstBinding
 
@@ -14,7 +16,7 @@ import com.example.openyourworld.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    private var TAG: String = javaClass.simpleName
     private var _binding: FragmentFirstBinding? = null
     // A reference to the service used to get location updates.
     private var mService: LocationTrackingService? = null;
@@ -34,12 +36,20 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.e(TAG, "onViewCreated")
+
+        // Find the ComposeView and set the content
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.setContent {
+            // Get location updates here
+            Log.d(TAG, "Getting location updates started")
+            //todo: add log of current location
+            mService?.BgLocationAccessScreen()
+            Log.d(TAG, "Getting location updates ended")
+        }
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            // Get location updates here
-            // Todo: fix composable annotation
-            mService?.BgLocationAccessScreen()
         }
     }
 
