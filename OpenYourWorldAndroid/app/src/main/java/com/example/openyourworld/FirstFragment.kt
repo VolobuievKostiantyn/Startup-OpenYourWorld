65253f3f4cd86f88ab62efcd589fcd3e758947ea
Kostiantyn Volobuiev <volobuiev.kostiantyn@gmail.com>
2025-09-20 18:14:47 +0300

    chore: move todo to git tasks

--- OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/FirstFragment.kt
+++ OpenYourWorldAndroid/app/src/main/java/com/example/openyourworld/FirstFragment.kt
@@ -83,7 +83,6 @@ class FirstFragment : Fragment() {
             // Mark area for two positions from the DB on map
             // Todo: emulate GPS movement in emulator based on https://github.com/VolobuievKostiantyn/Startup-OpenYourWorld/commit/2cdf32642ed41c200d38b3e948a549ec070c9af3 and add it to the pptx demo slide
             // Todo: hide all area and then unhide the way
-            // Todo: add git secure actions https://docs.github.com/en/actions/reference/security/secure-use
             val radiusInMeters = 5.0
             val latitude = LocationTrackingService.GlobalVariables.latitude
             val longitude = LocationTrackingService.GlobalVariables.longitude
